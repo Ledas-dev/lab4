792affd9b8df5a494b3072bf93576b94cf925a95
Lagumir <Legals92@mail.ru>
2020-10-23 15:10:48 +0300

    lab4

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -9,17 +9,27 @@
 #include <conio.h>
 #include <stdbool.h>
 
-void DFS(int i, int n);
-int M1[10][10], visited[10] = {0};
+void DFS(int p, int n);
+int **M1, *visited;
 
 int main(){
   srand(time(NULL));
-  int i = 0, j;
+  int p;
   float k = 0;
-  int n = rand()%10 + 3;
+  int n;
+  printf("Размер матрицы:\n");
+  scanf("%d", &n);
+  M1 = (int **)malloc(n*sizeof(int));
+  for(int i = 0; i<n;i++){
+    M1[i] = (int *)malloc(n*sizeof(int));
+  }
+  visited = (int *)malloc(n*sizeof(int));
+  for(int i = 0; i<n;i++){
+    visited[i] = 0;
+  }
   printf("Введем матрицу:\n");
-  for(i = 0;i<n;i++){
-    for(j = 0;j<i+1;j++){
+  for(int i = 0;i<n;i++){
+    for(int j = 0;j<i+1;j++){
       k = rand()%2;
       if(i != j){
         M1[i][j] = k;
@@ -36,14 +46,27 @@ int main(){
     printf("\n");
   }
   printf("\n");
-    DFS(0, n);
+
+  for(int i = 0;i<1;i++){
+  printf("Выберете вершину с которойъ начанете обход:");
+  scanf("%d", &p);
+    if(p<=n){
+      printf("\nВы проходите");
+
+    }else{
+      printf("Выберете новую координату\n");
+      i--;
+    }
+  }
+  printf("\n");
+    DFS(p, n);
 }
-void DFS(int i, int n){
+void DFS(int p, int n){
   int j;
-  printf("%d  ", i);
-  visited[i] = 1;
+  printf("%d  ", p);
+  visited[p] = 1;
   for(j=0; j<n;j++){
-    if(visited[j]==0 && M1[i][j] == 1){
+    if(visited[j]==0 && M1[p][j] == 1){
       DFS(j,n);
     }
   }
